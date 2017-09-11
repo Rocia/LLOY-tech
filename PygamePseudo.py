cc12e8ff46a55704913e63035e7e83d9894af4dd
ROCIA FERNANDES <rocia.fernandes@gmail.com>
2017-09-11 03:46:58 +0000

    Update PygamePseudo.py

--- PygamePseudo.py
+++ PygamePseudo.py
@@ -6,3 +6,75 @@ while running():
    update_screen()
 deinitialize()
 '''
+
+
+# Initialization phase
+# Setting the screen resolution and starting the game
+'''
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode((self.windowWidth,self.windowHeight), pygame.HWSURFACE)
+'''
+
+#Loading the image
+'''
+self._image_surf = pygame.image.load("pygame.png").convert()
+'''
+
+#On render method of drawing image on the screen
+'''
+    def on_render(self):
+        self._display_surf.blit(self._image_surf,(self.x,self.y))
+        pygame.display.flip()
+'''
+
+
+import pygame
+from pygame.locals import *
+ 
+class App:
+ 
+    windowWidth = 640
+    windowHeight = 480
+    x = 10
+    y = 10
+ 
+    def __init__(self):
+        self._running = True
+        self._display_surf = None
+        self._image_surf = None
+ 
+    def on_init(self):
+        pygame.init()
+        self._display_surf = pygame.display.set_mode((self.windowWidth,self.windowHeight), pygame.HWSURFACE)
+        self._running = True
+        self._image_surf = pygame.image.load("pygame.png").convert()
+ 
+    def on_event(self, event):
+        if event.type == QUIT:
+            self._running = False
+ 
+    def on_loop(self):
+        pass
+ 
+    def on_render(self):
+        self._display_surf.blit(self._image_surf,(self.x,self.y))
+        pygame.display.flip()
+ 
+    def on_cleanup(self):
+        pygame.quit()
+ 
+    def on_execute(self):
+        if self.on_init() == False:
+            self._running = False
+ 
+        while( self._running ):
+            for event in pygame.event.get():
+                self.on_event(event)
+            self.on_loop()
+            self.on_render()
+        self.on_cleanup()
+ 
+if __name__ == "__main__" :
+    theApp = App()
+    theApp.on_execute()
