daf8969a98a3678fd16b7cde0536b86003539b4e
ROCIA FERNANDES <rocia.fernandes@gmail.com>
2017-09-27 10:58:47 +0530

    TextAdventure Python game

---
+++ textAdventure.py
@@ -0,0 +1,15 @@
+
+The Goal: Remember Adventure? Well, we’re going to build a more basic version of that. A complete text game, the program will let users move through rooms based on user input and get descriptions of each room. To create this, you’ll need to establish the directions in which the user can move, a way to track how far the user has moved (and therefore which room he/she is in), and to print out a description. You’ll also need to set limits for how far the user can move. In other words, create “walls” around the rooms that tell the user, “You can’t move further in this direction.”
+
+Concepts to keep in mind:
+
+    Strings
+    Variables
+    Input/Output
+    If/Else Statements
+    Print
+    List
+    Integers
+
+
+The tricky parts here will involve setting up the directions and keeping track of just how far the user has “walked” in the game. I suggest sticking to just a few basic descriptions or rooms, perhaps 6 at most. This project also continues to build on using userinputted data. It can be a relatively basic game, but if you want to build this into a vast, complex word, the coding will get substantially harder, especially if you want your user to start interacting with actual objects within the game. That complexity could be great, if you’d like to make this into a longterm project. *Hint hint.
