097258245e79314e8a05c875e2d3608fc6818aba
ROCIA FERNANDES <rocia.fernandes@gmail.com>
2017-09-27 10:58:32 +0530

    Mad lib Generator

---
+++ madLibsGenerator.py
@@ -0,0 +1,12 @@
+
+The Goal: Inspired by Summer Son’s Mad Libs project with Javascript. The program will first prompt the user for a series of inputs a la Mad Libs. For example, a singular noun, an adjective, etc. Then, once all the information has been inputted, the program will take that data and place them into a premade story template. You’ll need prompts for user input, and to then print out the full story at the end with the input included.
+
+Concepts to keep in mind:
+
+    Strings
+    Variables
+    Concatenation
+    Print
+
+
+A pretty fun beginning project that gets you thinking about how to manipulate userinputted data. Compared to the prior projects, this project focuses far more on strings and concatenating. Have some fun coming up with some wacky stories for this!
